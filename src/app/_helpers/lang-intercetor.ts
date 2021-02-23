aacb582733a26249dcf3db5446e5772403dffe67
Florencia <mflorencia@izertis.com>
2021-02-23 11:43:14 +0100

    [SEMANTMURC-575] - Agregado de un interceptor para poder hacer llamadas multi idioma

---
+++ src/app/_helpers/lang-intercetor.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpInterceptor, HttpEvent, HttpRequest, HttpHandler } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { TranslateHelperService } from '../_services/translate-helper.service';
+
+@Injectable()
+export class LangInterceptor implements HttpInterceptor {
+    constructor(private translateHelper: TranslateHelperService) { }
+    intercept(httpRequest: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        let cloneReq;
+        let localeLang = '';
+        this.translateHelper.getLocalLang() ? localeLang = this.translateHelper.getLocalLang() : localeLang = 'es';
+        if (!httpRequest.urlWithParams.includes('json')) {
+            cloneReq = httpRequest.clone({ url: httpRequest.urlWithParams + '&language=@' + localeLang });
+        } else {
+            cloneReq = httpRequest;
+        }
+
+        return next.handle(cloneReq);
+    }
+}
