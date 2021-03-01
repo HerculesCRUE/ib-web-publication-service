14f7a34b71aaf96059ef4e7394d4a0a731dbebb0
pteran <pteran@izertis.com>
2021-03-01 09:14:01 +0100

    [SEMANTMURC-581] Dejar preparado back y front para dar soporte a consultas federadas Tests

--- src/app/sparqleditor/sparqleditor.component.spec.ts
+++ src/app/sparqleditor/sparqleditor.component.spec.ts
@@ -2,6 +2,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SPARQLEditorComponent } from './sparqleditor.component';
 import { TestingHelper } from '../_helpers/testing.spec';
+import { yasgui } from 'src/environments/environment';
 
 describe('SPARQLEditorComponent', () => {
   let component: SPARQLEditorComponent;
@@ -19,5 +20,28 @@ describe('SPARQLEditorComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+    expect(component.yasqe).toBeDefined();
+
+    expect(component.yasqe.config.requestConfig.endpoint).toBe(yasgui.endpoint);
+    expect(component.yasqe.config.requestConfig.method).toBe(yasgui.method);
+  });
+
+  describe('federChangeCheck', () => {
+    it('federChangeCheck with false value', () => {
+      component.federChangeCheck(false);
+      expect(component.yasqe).toBeDefined();
+
+      expect(component.yasqe.config.requestConfig.endpoint).toBe(yasgui.endpoint);
+      expect(component.yasqe.config.requestConfig.method).toBe(yasgui.method);
+    });
+
+
+    it('federChangeCheck with true value', () => {
+      component.federChangeCheck(true);
+      expect(component.yasqe).toBeDefined();
+
+      expect(component.yasqe.config.requestConfig.endpoint).toBe(yasgui.endpointFeder);
+      expect(component.yasqe.config.requestConfig.method).toBe(yasgui.methodFeder);
+    });
   });
 });
