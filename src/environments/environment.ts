80a5bcfc8b8b6dbfb75d369092ee51eb34e8b451
pteran <pteran@izertis.com>
2021-03-02 14:34:53 +0100

    [SEMANTMURC-581] Dejar preparado back y front para dar soporte a consultas federadas Added call to federated

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -23,7 +23,7 @@ export const yasgui = {
   // Fuseki
   endpoint: 'http://localhost:8787/trellis/sparql',
   method: 'POST',
-  endpointFeder: 'http://localhost:8787/trellis/sparql',
+  endpointFeder: 'http://localhost:8787/trellis/sparqlfeder',
   methodFeder: 'POST',
 };
 
@@ -33,5 +33,7 @@ export const yasgui = {
  *
  * This import should be commented out in production mode because it will have a negative impact
  * on performance if an error is thrown.
+ *
+ * import 'zone.js/dist/zone-error';  // Included with Angular CLI.
  */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
+
