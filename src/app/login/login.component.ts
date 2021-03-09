b3c63d25026f64e2836c1d1a0fdc18137c6d7b40
Florencia <mflorencia@izertis.com>
2021-03-09 09:00:37 +0100

    [SEMANTMURC-526] - cambios redirect

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { DomSanitizer } from '@angular/platform-browser';
+import { Router } from '@angular/router';
 import { environment } from 'src/environments/environment';
 import { LoginService } from '../_services/login.service';
 /**
@@ -17,7 +18,8 @@ export class LoginComponent implements OnInit {
   loginUrl = this.sanitizer.bypassSecurityTrustResourceUrl(environment.keycloak.authorizationUri + '?client_id=account&redirect_uri=http%3A%2F%2Flocalhost%3A8080%2Fauth%2Frealms%2Fumasio%2Faccount%2Flogin-redirect&state=0%2Fea63479f-b2b6-4bd0-92a1-a6c3a5add14e&response_type=code&scope=openid');
   constructor(
     private loginService: LoginService,
-    private sanitizer: DomSanitizer
+    private sanitizer: DomSanitizer,
+    private router: Router
   ) { }
 
   ngOnInit(): void {
@@ -43,9 +45,8 @@ export class LoginComponent implements OnInit {
         localStorage.setItem('access_token', response.access_token);
         localStorage.setItem('refresh_token', response.refresh_token);
         localStorage.setItem('username', this.model.username);
-
+        this.router.navigate(['./']);
       }
-      console.log(response);
     }, error => {
       console.log('error', error);
     });
