37f49002a5a2d5c9595a7e0bf5e40e01345de911
Florencia <mflorencia@izertis.com>
2021-03-18 12:24:57 +0100

    [SEMANTMURC-613] - cambio error build

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -14,6 +14,7 @@ export const environment = {
     userInfoUri: 'http://localhost:8080/auth/realms/umasio/protocol/openid-connect/userinfo',
     tokenUri: 'http://localhost:8080/auth/realms/umasio/protocol/openid-connect/token',
     logout: 'http://localhost:8080/auth/realms/umasio/protocol/openid-connect/logout',
+    redirectUrl: 'https%3A%2F%2Fherculesasioizertis.desa.um.es%2Fauth%2Frealms%2Fumasio%2Faccount%2F%23%2Fpersonal-info'
   }
 };
 
