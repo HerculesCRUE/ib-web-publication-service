798aa476bae89feece1b7e798d2b4b3b44c78a4a
pteran <pteran@izertis.com>
2021-04-22 16:58:50 +0200

    organization detail screen data

--- src/app/_models/organizationDetail.ts
+++ src/app/_models/organizationDetail.ts
@@ -1,3 +1,15 @@
+/**
+ *
+ *
+ * @export
+ * @class OrganizationDetail
+ */
+/**
+ *
+ *
+ * @export
+ * @class OrganizationDetail
+ */
 export class OrganizationDetail {
 
     /**
@@ -37,6 +49,14 @@ export class OrganizationDetail {
      */
     hasContactInfo: string;
 
+    /**
+     *
+     *
+     * @type {string}
+     * @memberof OrganizationDetail
+     */
+    homepage: string;
+
     /** 
      * The description. 
      */
@@ -51,4 +71,73 @@ export class OrganizationDetail {
      * The type. 
      */
     type: string;
+
+    /**
+     *
+     *
+     * @type {string}
+     * @memberof OrganizationDetail
+     */
+    isStartup: string;
+
+    /**
+     *
+     *
+     * @type {string}
+     * @memberof OrganizationDetail
+     */
+    publicCompany: string;
+
+    freetextKeyword: string;
+
+    hasAcreditation: string;
+    /**
+     *
+     *
+     * @type {string}
+     * @memberof OrganizationDetail
+     */
+    hasPredecessorOrganization: string;
+    /**
+     *
+     *
+     * @type {string}
+     * @memberof OrganizationDetail
+     */
+    hasSuccessorOrganization: string;
+    /**
+     *
+     *
+     * @type {string}
+     * @memberof OrganizationDetail
+     */
+    hasReservable: string;
+    /**
+     *
+     *
+     * @type {string}
+     * @memberof OrganizationDetail
+     */
+    contains: string;
+    /**
+     *
+     *
+     * @type {string}
+     * @memberof OrganizationDetail
+     */
+    participates: string;
+    /**
+     *
+     *
+     * @type {string}
+     * @memberof OrganizationDetail
+     */
+    provides: string;
+    /**
+     *
+     *
+     * @type {string}
+     * @memberof OrganizationDetail
+     */
+    relatedBy: string;
 }
