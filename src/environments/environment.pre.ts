f071eebe4176944abf953d9b682a7e593042e93e
Hugo Pintado <hpintado@izertis.com>
2021-06-07 17:32:31 +0200

    entorno de pre

--- src/environments/environment.pre.ts
+++ src/environments/environment.pre.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    urlBase: 'https://app.herculesasioizertis.desa.um.es/api',
+    urlBase: 'https://app.linkeddata1test.um.es/api',
     urlWebsocket: '/gs-guide-websocket',
     oauthClientId: 'acme',
     oauthSecret: 'acmesecret',
@@ -10,16 +10,16 @@ export const environment = {
         clientId: 'login-app',
         scope: 'openid, profile',
         authorizationGrantType: 'password',
-        authUrl: 'https://herculesasioizertis.desa.um.es/auth',
-        authorizationUri: 'https://herculesasioizertis.desa.um.es/auth/realms/umasio/protocol/openid-connect/auth',
-        userInfoUri: 'https://herculesasioizertis.desa.um.es/auth/realms/umasio/protocol/openid-connect/userinfo',
-        tokenUri: 'https://herculesasioizertis.desa.um.es/auth/realms/umasio/protocol/openid-connect/token',
-        logout: 'https://herculesasioizertis.desa.um.es/auth/realms/umasio/protocol/openid-connect/logout',
-        redirectUrl: 'https%3A%2F%2Fherculesasioizertis.desa.um.es%2Fauth%2Frealms%2Fumasio%2Faccount%2F%23%2Fpersonal-info',
+        authUrl: 'https://authld1test.um.es/auth',
+        authorizationUri: 'https://authld1test.um.es/auth/realms/umasio/protocol/openid-connect/auth',
+        userInfoUri: 'https://authld1test.um.es/auth/realms/umasio/protocol/openid-connect/userinfo',
+        tokenUri: 'https://authld1test.um.es/auth/realms/umasio/protocol/openid-connect/token',
+        logout: 'https://authld1test.um.es/auth/realms/umasio/protocol/openid-connect/logout',
+        redirectUrl: 'https%3A%2F%2Fauthld1test.um.es%2Fauth%2Frealms%2Fumasio%2Faccount%2F%23%2Fpersonal-info',
         clientSecret: ''
     },
-    lpdUrl: 'https://ldp.herculesasioizertis.desa.um.es/',
-    appUrl: 'https://app.herculesasioizertis.desa.um.es',
+    lpdUrl: 'https://ldp.authld1test.um.es/',
+    appUrl: 'https://app.linkeddata1test.um.es',
     sgi: 'https://github.com/HerculesCRUE/ib-asio-docs-/tree/master/13-Backend_SGI_Software'
 };
 
@@ -30,8 +30,8 @@ export const yasgui = {
     // method: 'GET'
 
     // Fuseki
-    endpoint: 'https://app.herculesasioizertis.desa.um.es/api/trellis/sparql',
+    endpoint: 'https://app.linkeddata1test.um.es/api/trellis/sparql',
     method: 'POST',
-    endpointFeder: 'https://app.herculesasioizertis.desa.um.es/api/trellis/sparql',
+    endpointFeder: 'https://app.linkeddata1test.um.es/api/trellis/sparql',
     methodFeder: 'POST',
 };
