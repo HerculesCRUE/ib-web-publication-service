9d6d32c76bdf63275f638b50cd47ed1f92921691
Daniel Ruiz Santamaria <druiz@izertis.com>
2021-07-12 18:17:44 +0200

    solved domain in call to federation

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -6,7 +6,7 @@ export const environment = {
   oauthSecret: 'acmesecret',
   skipPort: '8080',
   node: 'um',
-  domain: 'ldp.herculesasioizertis.desa.um.es',
+  domain: 'ldpld1.um.es',
   keycloak: {
     realm: 'umasio',
     clientId: 'login-app',
