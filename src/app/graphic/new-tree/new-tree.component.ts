83bebae80444bb7ec5286f8243bfaf5258f3b76b
Florencia <mflorencia@izertis.com>
2021-07-29 11:16:51 +0200

    SEMANTMURC-704 - cambios en tree para limpieza y botón para disparar busqueda. corrección en color tabla

--- src/app/graphic/new-tree/new-tree.component.ts
+++ src/app/graphic/new-tree/new-tree.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
 import { AreasService } from 'src/app/_services/areas.service';
 
 /**
@@ -13,7 +13,8 @@ import { AreasService } from 'src/app/_services/areas.service';
   templateUrl: './new-tree.component.html',
   styleUrls: ['./new-tree.component.css']
 })
-export class NewTreeComponent implements OnInit {
+export class NewTreeComponent implements OnInit, OnChanges {
+
   /**
    *
    *
@@ -21,6 +22,8 @@ export class NewTreeComponent implements OnInit {
    * @memberof NewTreeComponent
    */
   @Output() filterChanged: EventEmitter<any> = new EventEmitter<any>();
+  @Output() searchFilter: EventEmitter<any> = new EventEmitter<any>();
+  @Input() clearTree;
   /**
    *
    *
@@ -42,6 +45,8 @@ export class NewTreeComponent implements OnInit {
    * @memberof NewTreeComponent
    */
   filter = [];
+  resetData = false;
+  resetDataArray: any;
   /**
    * Creates an instance of NewTreeComponent.
    * @memberof NewTreeComponent
@@ -54,6 +59,7 @@ export class NewTreeComponent implements OnInit {
   ngOnInit(): void {
     this.areasService.getAll().subscribe(data => {
       this.data = this.returnCorrectData(data);
+      this.resetDataArray = this.returnCorrectData(data);
       setTimeout(() => {
         this.loaded = true;
       }, 300);
@@ -83,6 +89,21 @@ export class NewTreeComponent implements OnInit {
 
 
     return result;
+
+  }
+
+  ngOnChanges(simpleChanges: SimpleChanges) {
+    console.log(this.clearTree);
+    if (this.clearTree >= 1) {
+      this.resetData = true;
+    }
+
+    this.filter = [];
+    this.searchFilter.emit(this.filter);
+    this.data = this.resetDataArray;
+    setTimeout(() => {
+      this.resetData = false;
+    }, 1);
 
   }
   /**
@@ -118,6 +139,10 @@ export class NewTreeComponent implements OnInit {
     }
     this.filter = this.filter.filter((v, i, a) => a.indexOf(v) === i);
     this.filterChanged.emit(this.filter);
+  }
+
+  search() {
+    this.searchFilter.emit(this.filter);
   }
 
   /**
